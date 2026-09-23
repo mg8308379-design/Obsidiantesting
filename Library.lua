d48f8f1f59c0556697aab27e64076ad309532746
mg8308379-design <mg8308379@gmail.com>
2026-09-23 05:03:23 +0100

    Library.lua

--- Library.lua
+++ Library.lua
@@ -7152,12 +7152,11 @@ end
         -- Move the tab's container into this float window
         TabContainer.Parent = FloatFrame
         TabContainer.Visible = true
-        TabContainer.Position = UDim2.fromOffset(0, 49)
-        TabContainer.Size = UDim2.new(1, -10, 1, -70)
-TabLeft.Position = UDim2.fromOffset(5, 0)
-TabLeft.Size = UDim2.new(0.5, -8, 1, 0)
+TabContainer.Position = UDim2.fromOffset(5, 49)
+TabContainer.Size = UDim2.new(1, -10, 1, -70)
 
 
+        
         -- Make it draggable (title bar drag)
         Library:MakeDraggable(FloatFrame, FloatTopBar, true)
 
